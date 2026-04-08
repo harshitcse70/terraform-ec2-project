0c0d6ee9dec67c8d7dd7145fe850c8145ecf8602
Harshit <harshitcse70@gmail.com>
2026-04-08 06:53:04 +0000

    Initial Terraform EC2 setup

---
+++ main.tf
@@ -0,0 +1,44 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "app_server" {
+  ami                    = "ami-0d593311db5abb72b"
+  instance_type          = "t3.micro"
+  key_name               = "test112"
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+
+  tags = {
+    Name = "Terraform_Demo"
+  }
+}
